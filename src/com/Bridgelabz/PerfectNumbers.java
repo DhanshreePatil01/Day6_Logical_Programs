532f93a6d989d0a7652515bf4bb88a7697cf09c0
DhanshreePatil01 <patildhanshrees6112@gmail.com>
2022-11-22 00:28:35 +0530

    Started Day6 practice problems.

---
+++ src/com/Bridgelabz/PerfectNumbers.java
@@ -0,0 +1,32 @@
+package com.Bridgelabz;
+
+import java.util.Scanner;
+
+public class PerfectNumbers {
+	
+public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number : ");
+		int num = sc.nextInt();
+		
+		int sum=0;
+		
+		for(int i=1;i<=num/2;i++)
+		{
+			if(num%i==0)
+			{
+				sum=sum+i;
+			}
+		}
+		
+		if(sum==num)
+		{
+			System.out.println(num+" is perfect number");
+		}
+		else
+		{
+			System.out.println(num+" is not perfect number");
+		}
+ }
+}
